9c9f806cc9ab0cb9aa69e7aa346697c469613abe
krushna <krushnadeore666@gmail.com>
2026-06-30 11:43:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,2 @@
 //your JS code here. If required.
-function secondHighest(arr) {
-  if (arr.length < 2) return -Infinity;
 
-  let highest = -Infinity;
-  let second = -Infinity;
-
-  for (let num of arr) {
-    if (num > highest) {
-      second = highest;
-      highest = num;
-    } else if (num > second && num < highest) {
-      second = num;
-    }
-  }
-
-  return second;
-}
